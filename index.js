9008982bce1419a026e08864a347e4136d800e0e
linhan.cai <linhan.cai@outlook.com>
2019-11-10 23:24:49 +0800

    cache

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
+require('dotenv').config();
 const express = require('express');
 const {explain} = require("./library/YouDaoExplain")
 
@@ -28,5 +26,51 @@ app.get('/word', async (req, res) => {
         console.log(e.message)
     }
 });
+
+/*
+create table wordList (
+    id int primary key auto_increment,
+    name varchar(64) not null default '',
+    remark varchar(255) not null default ''
+) 
+
+create table wordElements (
+    id int primary key auto_increment,
+    word varchar(64) not null default '',
+)
+
+create table wordListElements (
+    id int primary key auto_increment,
+    list_id int not null default 0,
+    element_id int not null default 0
+)
+
+create table wordElementLists (
+    id int primary key auto_increment,
+    element_id int not null default 0,
+    list_id int not null default 0
+)
+
+create table wordExplains (
+
+)
+
+
+ */
+app.get('/lists', async (req, res) => {})
+
+app.post('/lists', async(req, res) => {})
+
+app.put('/lists/{id}', async(req, res) => {})
+
+app.delete('/lists/{id}', async(req, res) => {})
+
+app.get('/lists/words', async(req, res) => {})
+
+app.post('/list/words', async(req, res) => {})
+
+app.get('/lists/words/{$id}', async(req, res) => {})
+
+app.delete('/list/words/{id}', async(req, res) => {})
 
 app.listen(8000, ()=>{ console.log("server start success") })
